4cc8c96b88d21358e88093c87e63e4cc7b545672
Arisa <uliannaobr@gmail.com>
2023-04-13 22:58:32 +0400

    code cleanup

--- lab-3/lab-3.py
+++ lab-3/lab-3.py
@@ -9,7 +9,7 @@ def power(a, n):
 	return (a ** (n-1)) % n
 
 
-def modInverse(a, n):
+def test_Fermat(a, n):
 	print('checking if a number is prime: ', n, 'with number ', a)
 	if __gcd(a, n) != 1:
 		print("the number isn`t prime! :(\n")
@@ -18,7 +18,7 @@ def modInverse(a, n):
 		print('remainder of the division ', result)
 		if result != 1:
 			a += 1
-			modInverse(a, n)
+			test_Fermat(a, n)
 		else:
 			print('the number is prime! :)\n')
 
@@ -27,6 +27,6 @@ a = 3
 n = 11
 max = n + 5
 while n < max:
-	modInverse(a, n)
+	test_Fermat(a, n)
 	n += 1
 
